3ee38d184e3b84dcdd50b87cda7e269a2338f197
leloc0609 <lelocofficial@gmail.com>
2021-09-17 02:27:39 +0700

    ex3 intial

---
+++ Core/Inc/traffic_light.h
@@ -0,0 +1,13 @@
+/*
+ * traffic_light.h
+ *
+ *  Created on: Sep 17, 2021
+ *      Author: Win10
+ */
+
+#ifndef INC_TRAFFIC_LIGHT_H_
+#define INC_TRAFFIC_LIGHT_H_
+
+void cross4Way(void);
+
+#endif /* INC_TRAFFIC_LIGHT_H_ */
